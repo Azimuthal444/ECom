bdf300e052c0ce29cda1d215f10cb53366b3780f
Vignesh V Pai <vigneshpai444@gmail.com>
2021-05-24 09:38:27 +0530

    Created login form partially

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,3 +1,8 @@
-const colors = {white: '#FFFFFF', doveGray: '#707070', black: '#000'};
+const colors = {
+  white: '#FFFFFF',
+  doveGray: '#707070',
+  black: '#000',
+  cornflowerBlue: '#6881e7',
+};
 
 export default colors;
--- src/screens/LogInScreen/index.js
+++ src/screens/LogInScreen/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Controller, useForm} from 'react-hook-form';
 import {StyleSheet, Text, View} from 'react-native';
+import {Input} from 'react-native-elements';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {connect} from 'react-redux';
-import CustomInput from '../../components/CustomInput';
+import CustomButton from '../../components/CustomButton';
+
 import colors from '../../constants/colors';
 import fonts from '../../constants/fonts';
 
 const LogInScreen = ({
   languageModel: {logIn, email: emailLabel, password: passwordLabel},
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
   const {control} = useForm();
 
   return (
@@ -19,9 +23,11 @@ const LogInScreen = ({
         <Controller
           control={control}
           render={({field: {onBlur, onChange, value}}) => (
-            <CustomInput
+            <Input
               placeholder={emailLabel}
-              {...{onBlur, onChange, value}}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              label={emailLabel}
             />
           )}
           name="email"
@@ -30,14 +36,23 @@ const LogInScreen = ({
         <Controller
           control={control}
           render={({field: {onBlur, onChange, value}}) => (
-            <CustomInput
-              placeholder={emailLabel}
-              {...{onBlur, onChange, value}}
+            <Input
+              placeholder={passwordLabel}
+              secureTextEntry={!showPassword}
+              label={passwordLabel}
+              rightIcon={
+                <Icon
+                  name={showPassword ? 'visibility-off' : 'visibility'}
+                  size={20}
+                  onPress={() => setShowPassword(prevState => !prevState)}
+                />
+              }
             />
           )}
           name="password"
           defaultValue=""
         />
+        <CustomButton style={styles.buttonStyle} title={logIn} />
       </View>
     </SafeAreaView>
   );
@@ -57,5 +72,10 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontFamily: fonts.montserratMedium,
   },
-  loginFormContainer: {marginTop: 60, paddingHorizontal: 10},
+  loginFormContainer: {
+    marginTop: 60,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+  },
+  buttonStyle: {width: '90%', marginTop: 20},
 });
